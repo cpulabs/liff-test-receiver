92b23f60fb70482c7f373affb86731d4899824f1
ito.takahiro <ito.takahiro@linecorp.com>
2020-04-13 19:33:24 +0900

    add liffid

--- liff-starter.js
+++ liff-starter.js
@@ -1,8 +1,9 @@
 window.onload = function() {
     const useNodeJS =
         false;  // if you are not using a node server, set this value to false
-    const defaultLiffId = '???';  // change the default LIFF value if you are
-                                  // not using a node server
+    const defaultLiffId =
+        '1654077833-jQJZpGBZ';  // change the default LIFF value if you are
+                                // not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = '';
